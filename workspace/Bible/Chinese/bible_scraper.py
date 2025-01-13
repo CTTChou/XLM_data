74e36538215aa2c42f6c4878baee2873e452f7ba
tienchenlee <appielee4305@gmail.com>
2025-01-13 19:49:02 +0800

    upload chinese bible demo and add docstring

--- workspace/Bible/Chinese/bible_scraper.py
+++ workspace/Bible/Chinese/bible_scraper.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import json
+import os
 from bs4 import BeautifulSoup
 from pprint import pprint
 from selenium import webdriver
@@ -10,6 +11,7 @@ from urllib.parse import unquote
 def get_ChiBibleDICT(url):
     """
     爬取指定網址的中文聖經資料，並將其整理成字典格式。
+    
     參數:
     url (str): 需要爬取的網頁地址，通常為中文聖經的閱讀頁面。
 
@@ -70,6 +72,16 @@ def get_ChiBibleDICT(url):
     return ChiBibleDICT
 
 def get_BookLIST(url):
+    """
+    使用 Selenium 驅動 Chrome 瀏覽器來訪問指定的 URL，並抓取 HTML 原始碼。
+    使用 BeautifulSoup 解析 HTML，從 `<select>` 標籤中提取所有書籍的選項值，並返回這些值作為書籍列表。
+
+    參數:
+        url (str): 目標網頁的 URL。
+
+    返回:
+        list: 包含所有書籍選項值的列表。
+    """    
     driver = webdriver.Chrome()
     driver.get(url)
     driver.implicitly_wait(10)
@@ -82,21 +94,34 @@ def get_BookLIST(url):
     return bookLIST
 
 def main(url):
+    """
+    獲取指定 URL 的書籍列表，並對每本書進行處理，抓取每章的中文聖經內容。
+    將每本書的內容存儲為單獨的 JSON 文件，最後將所有書的內容匯總為一個 JSON 文件。
+
+    參數:
+        url (str): 包含書籍列表的目標網頁的 URL。
+
+    返回:
+        list: 包含所有書籍中文聖經內容的列表。
+    """    
     bookLIST = get_BookLIST(url)
     all_ChiBibleLIST = []
     for b in bookLIST:
         ChiBibleLIST = []
         bookname = unquote(b)
         try:
-            for i in range(1, 151):
+            for i in range(1, 51):
                 ch_url = f"https://bible.fhl.net/new/read.php?VERSION4=tcv95&strongflag=0&TABFLAG=1&chineses={b}&chap={i}&submit1=%E9%96%B1%E8%AE%80"
                 ChiBibleDICT = get_ChiBibleDICT(ch_url)
                 pprint(ChiBibleDICT)
                 ChiBibleLIST.append(ChiBibleDICT)
         except IndexError:
-            pass
+            continue
         
-        book_jsonFILE = f"{bookname}.json"
+        book_folder = "../../../data/Bible/Chinese/book"
+        if not os.path.exists(book_folder):
+            os.makedirs(book_folder)
+        book_jsonFILE = f"../../../data/Bible/Chinese/book/{bookname}.json"
         with open(book_jsonFILE, "w", encoding="utf-8") as f:
             json.dump(ChiBibleLIST, f, ensure_ascii=False, indent=4)
             all_ChiBibleLIST.extend(ChiBibleLIST)
