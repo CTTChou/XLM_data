3efe13acd47feb8b651f547e67373b1645e3d7af
emilyC <ganpeijie3@gmail.com>
2025-01-19 10:43:06 -0700

    update docstring

--- workspace/Bible/English/eng_bible_scraper.py
+++ workspace/Bible/English/eng_bible_scraper.py
@@ -7,98 +7,50 @@ from bs4 import BeautifulSoup
 import json
 import requests
 import re
-
-
-def find_lines(html_doc):
-    pattern_start = r'id="[^"]*-([0-9]+)"'
-    match_start = re.findall(pattern_start, html_doc)
-    end_line_num = int(match_start[0]) + len(match_start) -1
-    return match_start[0], end_line_num
     
     
-def search(htmlSTR, chapter, book):
+def search(htmlSTR):
     """
-    Searches the specified chapter in the htmlSTR by obtaining the starting and 
-    ending lines and returns the content and line number list.
+    Searches the the htmlSTR and returns the content and line number list.
     
     Parameters:
         htmlSTR (str): The HTML string.
-        chapter (int): The specified chapter.
     
     Returns:
         list: The list of searched content.
         list: The list of line numbers.
     """
-    
     soup = BeautifulSoup(htmlSTR, 'lxml')
-    
-    o = []
+    tempLIST = []
     paragraph_div = soup.find('div', class_="version-GNT result-text-style-normal text-html")
     ps = paragraph_div.find_all('p')
     for j in ps:
         result = j.find_all('span', recursive=False)
         for i in result:
-            o.append(i.get_text())
-    print(o)
-    
+            tempLIST.append(i.get_text())
     outputLIST = []
-    for i in o:
+    for i in tempLIST:
         if "\xa0" in i:
             outputLIST.append(i)
         else:
             outputLIST[-1] += " "
-            outputLIST[-1] += i
-    print (outputLIST)   
-    
+            outputLIST[-1] += i  
     lineLIST = []
     for i, j in enumerate(outputLIST):
         pattern_start = r'(\d+)\xa0'
         match = re.findall(pattern_start, j)
         print(match[0])
         lineLIST.append(match[0])
-        outputLIST[i] = re.sub(r'\d+\xa0|\[\w\]|\(\w\)', "", j)
-    lineLIST[0] = "1"
-    print(lineLIST)
-    print (outputLIST)    
-    
-    '''outputLIST = []
-    lineLIST = []
-    start_line = find_lines(htmlSTR)[0]         #calls the find_lines(html_doc) function to get starting and ending lines.
-    end_line = find_lines(htmlSTR)[1]
-    htmlSTR = re.sub(r'<h3[^>]*>.*?</h3>', "", htmlSTR)
-    soup = BeautifulSoup(htmlSTR, "lxml")
-    
-    first_line = soup.find_all('span', class_= f"text Gen-{chapter}-1")             # Gen - to be fixed !!
-    for l in first_line:
-        tempSTR = l.get_text()
-        tempSTR = re.sub("\[\w\]|\(\w\)|^(\d|-|\)|\(){1,5}\s", "", tempSTR)
-        outputLIST.append(tempSTR)
-        lineLIST.append(1)
-        
-    for start_line in range(end_line+1):
-        result = soup.find_all('span', id= f"en-GNT-{start_line}")  
-        for k in result:
-            tempSTR = k.get_text()
-            pattern = r'(\d+-\d+|\d+)'
-            line = re.findall(pattern, tempSTR)   
-            tempSTR2 = re.sub("\[\w\]|\(\w\)|^(\d|-|\)|\(){1,5}\s", "", tempSTR)
-            outputLIST.append(tempSTR2)
-            lineLIST.append(line[0])'''
-    
-    
-            
-    '''for j in outputLIST:
-        if chapter < 10:
-            outputLIST[outputLIST.index(j)] = re.sub("\[\w\]|\(\w\)|^..", "", j)   
-        else:
-            outputLIST[outputLIST.index(j)] = re.sub("\[\w\]|\(\w\)|^...", "", j)  '''    
+        outputLIST[i] = re.sub(r'\d+\xa0|\[\w\]|\(\w\)|\(|\)|\“|\‘|\’|\”|\s$', "", j)
+    lineLIST[0] = "1"   
+      
     return outputLIST, lineLIST
 
 
-def main(url, chapter_num, book): 
+def main(url, chapter_num): 
     """
-    Fetches a web page from the provided URL, extracts content, 
-    calls the search() function to obtain content of the specified chapter, and returns the result list.
+    Fetches a web page from the provided URL, extracts the html string, calls the 
+    search() function to obtain content of the specified chapter, and returns the result list.
     
     Parameters:
         url (str): The input url string.
@@ -109,7 +61,7 @@ def main(url, chapter_num, book):
     """
     response = requests.get(url)
     html_doc = response.text  
-    searched_results = search(html_doc, chapter_num, book)
+    searched_results = search(html_doc)
     engLIST = searched_results[0]
     lineLIST = searched_results[1]
     
@@ -145,16 +97,16 @@ if __name__ == "__main__":
     
     for n in bookLIST:
         resultLIST = []
-        for j in range(1,151):           #還需加上怎麼找有幾個章節的部分
+        for j in range(1,151):           #還可以加上怎麼找有幾個章節的部分
             try:
                 url = f"https://www.biblegateway.com/passage/?search={n}%20{j}&version=GNT"
-                resultLIST.append(main(url, j, n))
+                resultLIST.append(main(url, j))
                 print(resultDICT)            
             except Exception:
                 pass
         resultDICT = {}
         resultDICT[n] = resultLIST
-        #dataLIST.append(resultDICT)
+
         with open("../../../data/Bible/English/book/genesis.json", "r", encoding="utf-8") as f:
             dataLIST = json.load(f)
             dataLIST.append(resultDICT)        
