d04f736390bb328452c54defae93575ed9101332
tienchenlee <appielee4305@gmail.com>
2025-02-21 18:51:50 +0800

    upload name_checker for selecting names we need

---
+++ workspace/Bible/English/eng_name_checker.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import json
+from pprint import pprint
+
+def main():
+    """
+    讀取 JSON 格式的英文聖經數據，並提取所有經文內容。
+
+    此函式會執行以下步驟：
+    1. 從指定路徑 ("../../../data/Bible/English/all_EngBible.json") 讀取 JSON 數據。
+    2. 遍歷 JSON 數據結構，提取每卷書的所有經文。
+    3. 將經文內容存入列表 `all_versesLIST`，並返回該列表。
+
+    回傳:
+        list: 包含所有經文內容的列表，每個元素為一節經文的文字內容。
+    """ 
+    with open("../../../data/Bible/English/all_EngBible.json", "r", encoding="utf-8") as f:
+        dataLIST = json.load(f)     
+    all_versesLIST =[]
+    for bookDICT in dataLIST:
+        bookLIST = list(bookDICT.keys())   #拿到書名 
+        for versesLIST, chapterDICT in enumerate(bookDICT[bookLIST[0]]):        
+            versesLIST = list(chapterDICT.values())[0]  # 取得 values 的第一個元素（列表）
+            for verseDICT in versesLIST:
+                all_versesLIST.append(next(iter(verseDICT.values())))  # 加入 verse 內文
+            #pprint(all_versesLIST)
+    return all_versesLIST
+
+
+if __name__ == "__main__":
+    all_versesLIST = main()
+    versesSTR = " ".join(all_versesLIST) #把所有內文接在一起
+    with open("../../../data/Bible/English/english_persons.json", "r", encoding="utf-8") as f:
+        nameLIST = json.load(f)
+        print(f"可檢查的人名數量：{len(nameLIST)}")
+        not_haveLIST = []
+        for name in nameLIST:
+            if name not in versesSTR:   #比對 english_persons.json 裡的人名是否在 all_EngBible.json
+                not_haveLIST.append(name)  #不需要的人名 
+        print(f"不需要的人名數量：{len(not_haveLIST)}")
+        pprint(not_haveLIST)
