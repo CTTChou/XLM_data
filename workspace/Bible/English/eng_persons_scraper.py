ad1057e86ef3dd5b2cc9ba46117c30bd41920049
emilyC <ganpeijie3@gmail.com>
2025-02-21 19:01:14 +0800

    update language

--- workspace/Bible/English/eng_persons_scraper.py
+++ workspace/Bible/English/eng_persons_scraper.py
@@ -11,17 +11,13 @@ def search(htmlSTR):
     soup = BeautifulSoup(htmlSTR, 'lxml')
     line = soup.find_all('li')
     ch_personLIST = []
-    eng_personLIST = []
     
     for i in line:
         pattern_ch = r'（(.*)）'
-        pattern_eng = r' ?(.*) （'
         ch_person = re.findall(pattern_ch, i.get_text())
         ch_personLIST.append(ch_person[0])
-        eng_person = re.findall(pattern_eng, i.get_text())  
-        eng_personLIST.append(eng_person[0])
     
-    return ch_personLIST, eng_personLIST
+    return ch_personLIST
 
 
 def main(url): 
@@ -29,9 +25,8 @@ def main(url):
     response.encoding = 'Big5'
     html_doc = response.text  
     searched_results = search(html_doc)    
-    engLIST = searched_results[1]
-    chLIST = searched_results[0]
-    return engLIST, chLIST
+    chLIST = searched_results
+    return chLIST
     
 
 if __name__ == "__main__":
@@ -39,15 +34,7 @@ if __name__ == "__main__":
     for i in letterLIST:
         url_main = f"http://www.christianstudy.com/biography_names_{i}.html"
         results = main(url_main)
-        engLIST = results[0]
-        chLIST = results[1]
-        
-        with open("../../../data/Bible/English/english_persons.json", "r", encoding="utf-8") as f:
-            old_engLIST = json.load(f)    
-        with open("../../../data/Bible/English/english_persons.json", "w", encoding="utf-8") as f:
-            for i in engLIST:
-                old_engLIST.append(i)            
-            json.dump(old_engLIST, f, ensure_ascii=False, indent=4)   
+        chLIST = results
         
         with open("../../../data/Bible/Chinese/chinese_persons.json", "r", encoding="utf-8") as f:
             old_chLIST = json.load(f)       
