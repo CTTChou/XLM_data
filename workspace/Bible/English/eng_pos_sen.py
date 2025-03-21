1de4cb6e36870c0d99c7775a722c9854c805051d
ellenyp <ivickie621@gmail.com>
2025-03-21 15:08:59 +0800

    test response

--- workspace/Bible/English/eng_pos_sen.py
+++ workspace/Bible/English/eng_pos_sen.py
@@ -28,7 +28,7 @@ def articutEN(inputSTR: str) -> list:
     }    
    
     #response = post(url, json=payload).json()
-    response = post("{}/Articut_EN/BulkAPI/".format(url), json=payload)
+    response = post("{}/ArticutEN/BulkAPI/".format(url), json=payload)
     print(response)
     return response
 
